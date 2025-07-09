574ca60b911a7e751bea2e48bbd2635fa4f6697c
MemuSAE <momok89889@gmail.com>
2025-07-09 15:06:01 +0300

    Encode-and-Decode-Strings.py

--- Array & Hashing/Encode-and-Decode-Strings.py
+++ Array & Hashing/Encode-and-Decode-Strings.py
@@ -1,7 +1,7 @@
 class Solution:
     @staticmethod
     def encode(strs: list[str]) -> str:
-        # Encode each string as length:string with # as separator
+        
         return '#'.join(f'{len(s)}:{s}' for s in strs)
     
     @staticmethod
@@ -12,13 +12,13 @@ class Solution:
         result = []
         i = 0
         while i < len(s):
-            # Find colon to get length
+            
             colon_idx = s.find(':', i)
             if colon_idx == -1:
                 break
-            # Extract length
+            
             length = int(s[i:colon_idx])
-            # Extract string of specified length
+            
             result.append(s[colon_idx + 1:colon_idx + 1 + length])
-            i = colon_idx + 1 + length + 1  # Move past string and # separator
+            i = colon_idx + 1 + length + 1 
         return result
