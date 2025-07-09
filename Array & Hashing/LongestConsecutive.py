0e7f26f22fd625f10cf0ee07c224795efb24e8c6
MemuSAE <momok89889@gmail.com>
2025-07-09 15:05:28 +0300

    edit

--- Array & Hashing/LongestConsecutive.py
+++ Array & Hashing/LongestConsecutive.py
@@ -5,12 +5,12 @@ class Solution:
         longest = 0
         
         for num in num_set:
-            # Only start checking sequences from the smallest number in the sequence
+            
             if num - 1 not in num_set:
                 current = num
                 current_streak = 1
                 
-                # Count consecutive numbers
+                
                 while current + 1 in num_set:
                     current += 1
                     current_streak += 1
