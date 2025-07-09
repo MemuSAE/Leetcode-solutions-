c019db97e8bafeafae3fd2bb71a016f5d2d2a362
MemuSAE <momok89889@gmail.com>
2025-07-09 15:05:05 +0300

    edit

--- Array & Hashing/Products-of-Array-Except-Self.py
+++ Array & Hashing/Products-of-Array-Except-Self.py
@@ -4,13 +4,13 @@ class Solution:
         n = len(nums)
         output = [1] * n
         
-        # Calculate products of all elements to the left of each index
+        
         left_product = 1
         for i in range(n):
             output[i] = left_product
             left_product *= nums[i]
         
-        # Multiply by products of all elements to the right of each index
+        
         right_product = 1
         for i in range(n - 1, -1, -1):
             output[i] *= right_product
