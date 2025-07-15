70576cfb53b3c3bba0d876b1f79bef79fc0ff827
MemuSAE <momok89889@gmail.com>
2025-07-15 23:37:37 +0300

    validword.py

---
+++ Valid_Word.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < 3:
+            return False
+        
+        has_vowel = False
+        has_consonant = False
+        for char in word:
+            if not (char.isalnum()):
+                return False
+            if char.lower() in 'aeiou':
+                has_vowel = True
+            elif char.isalpha():
+                has_consonant = True
+        return has_vowel and has_consonant
